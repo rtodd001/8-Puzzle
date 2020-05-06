ff382721ec3a848764d191cb27bd78af59d0b828
rtodd001 <rtodd001@ucr.edu>
2020-05-06 11:12:57 -0700

    changes the search to support the new node class

--- Search.py
+++ Search.py
@@ -1,45 +1,57 @@
 import heapq
 import numpy as np
+from Node import *
 
 class Search:
     def solve(self, problem, heuristic):
         counter = 0
+        foundCount = 0
         maxSize = -1
         frontier = []
-        heapq.heappush(frontier,(0, problem.getInitState()))
+        node = Node(problem.getInitState(),1)
+        print("Cost", node.getCost())
+        heapq.heappush(frontier,(node.getCost(), node))
         print ("Expanding state")
         problem.print()
-        #for i in range(0,2):
         while True:
             #do something
             #0: Cost
             #1: Puzzle
 
             item = heapq.heappop(frontier)
-            hn = heuristic.calculate(item[1], problem.getGoalState())
-            print("The best state to expand with g(n) = {} and h(n) = {} is…".format(item[0] - hn, hn))
-            problem.setCurrentState(item[1])
+            #print("Node ", item)
+            hn = heuristic.calculate(item[1].getMatrix(), problem.getGoalState())
+            print("The best state to expand with g(n) = {} and h(n) = {} is…".format(item[1].getDepth(), hn))
+            problem.setCurrentState(item[1].getMatrix())
             problem.print()
             print("Expanding this node")
             counter += 1
             #print("Front: ", frontier)
             #print(item[0], item[1])
     
-            if(np.array_equal(item[1], problem.getGoalState())):
-                problem.setCurrentState(item[1])
+            if(np.array_equal(item[1].getMatrix(), problem.getGoalState())):
+                problem.setCurrentState(item[1].getMatrix())
                 print("GOAL!!!")
                 print("To solve this problem the search algorithm expanded a total of {} nodes.\nThe maximum number of nodes in the queue at any one time: {}.".format(counter, maxSize))
                 break
             #returns all the possible moves
-            options = problem.getLegalOps(item[1])
+            options = problem.getLegalOps(item[1].getMatrix())
+
             #iterate through all the posible moves
             #This is the expansion of the removed node
             for one in options:
                 #create a new matrix with the new possible move
-                possibility = problem.moveTile(item[1], one)
+                possibility = problem.moveTile(item[1].getMatrix(), one)
+                temp = Node(possibility, item[1].getDepth())
+                temp.setDepth(item[1].getDepth() + 1)
                 problem.setCurrentState(possibility)
-                cost = item[0] + 1 + heuristic.calculate(possibility, problem.getGoalState())
-                heapq.heappush(frontier,(cost, possibility))
+                cost = temp.getDepth() + heuristic.calculate(possibility, problem.getGoalState())
+                temp.setCost(cost)
+                #print("Cost: ", temp.getCost())
+                heapq.heappush(frontier,(temp.getCost(), temp))
+            print("Done")
+            """ for index in frontier:
+                print(index[0], index[1]) """
             maxSize = max(maxSize, len(frontier))
                 
             
