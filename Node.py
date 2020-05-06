ee5264c3b96857c58d300d2553b5706420d03668
rtodd001 <rtodd001@ucr.edu>
2020-05-06 11:09:53 -0700

    Created a node object that can keep track its own depth and cost

---
+++ Node.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, matrix, dep):
+        self.state = matrix
+        self.cost = dep
+        self.depth = dep
+    
+    def getMatrix(self):
+        return self.state
+
+    def getCost(self):
+        return self.cost
+    
+    def getDepth(self):
+        return self.depth
+    
+    def setDepth(self, num):
+        self.depth = num
+
+    def setCost(self, num):
+        self.cost = num
+
+    def __lt__(self, other):
+        return self.cost < other.cost
