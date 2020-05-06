d664c90171077cc1bd5bcbe598b8650f9e68f1b8
rtodd001 <rtodd001@ucr.edu>
2020-05-05 22:05:52 -0700

    Fixed the bug that made move right operations not possible

--- Problem.py
+++ Problem.py
@@ -7,7 +7,15 @@ class Problem:
         self.expand_count = 0
         self.size = 3
         self.goal_state = np.array([[1,2,3],[4,5,6],[7,8,0]])
+
+    #------------------SETTER----------------#
+    def setInitState(self, matrix):
+        self.init_state = matrix
+        self.current_state = matrix
     
+    def setCurrentState(self, matrix):
+        self.current_state = matrix
+
     #------------------GETTER----------------#
     def getInitState(self):
         return self.initial_state
@@ -23,8 +31,9 @@ class Problem:
 
     #return a numpy array with all possible moves
     #values represent the location the 0 can go
-    def getLegalOps(self):
-        arr = np.array(self.current_state)
+    def getLegalOps(self, matrix):
+        #arr = np.array(self.current_state)
+        arr = np.array(matrix)
         indices = np.where(arr == 0)
         #print("Indices", indices)
         moves = [indices[0][0], indices[1][0]]
@@ -42,17 +51,25 @@ class Problem:
             allMoves.append([moves[0], moves[1] - 1])
 
         #Right
-        if moves[1] > self.size - 1:
+        if moves[1] < self.size - 1:
             allMoves.append([moves[0], moves[1] + 1])
 
         #print("Test ", allMoves[0], "Item ", self.initial_state[allMoves[0][0]][allMoves[0][1]])
-
         return allMoves
 
-    #------------------SETTER----------------#
-    def setInitState(self, matrix):
-        self.init_state = matrix
-        self.current_state = matrix
+    #----------------FUNCTIONS---------------#
+    def moveTile(self, matrix, position):
+        arr = np.array(matrix)
+        indices = np.where(arr == 0)
+        #location of the 0 tile
+        moves = [indices[0][0], indices[1][0]]
+        #The item that we are going to swap
+        update = arr[position[0], position[1]]
+        arr[position[0], position[1]] = 0
+        arr[moves[0], moves[1]] = update
+        return arr.tolist()
+
+
 
     #------------------PRINT-----------------#
     def print(self):
