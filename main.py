9b83d9b9ff9c1ec90bc817209c87a53f12bde254
rtodd001 <rtodd001@ucr.edu>
2020-05-05 22:06:49 -0700

    Doing some interface cleaning

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import numpy as np 
 from Problem import *
 from Heuristic import *
+from Search import *
+import heapq
 
 size = 3
-
-def printPuzzle(matrix):
-    for i in range (0,size):
-        print(matrix[i])
+temp = Problem()
 
 def defaultPuzzle():
     matrix = [[1,2,3],[4,8,0],[7,6,5]]
@@ -15,30 +14,33 @@ def defaultPuzzle():
 def menu():
     val = int(input("Welcome to 861228848 8 puzzle solver.\nType '1' to use a default puzzle, or '2' to enter your own puzzle.\n"))
     puzzle = []
+    uniform = UniformHeur()
+    misplace = MisplacedHeur()
+    euclid = EuclidHeur()
+    search = Search()
     if val == 1:
-        return defaultPuzzle()
+        puzzle = defaultPuzzle()
     elif val == 2:
         print("Enter your puzzle,use a zero to represent the blank")
         for i in range(0,size):
             x,y,z = input("Enter the row and use space or tabs between numbers ").split()
             puzzle.append([int(x),int(y),int(z)])
-        return puzzle
+    
+    problem = Problem(puzzle)
+    choice = int(input("Enter your choice of algorithm\nUniform Cost Search\nA* with the Misplaced Tile heuristic.\nA* with the Euclidian distance heuristic\n"))
+    if choice == 1:
+        search.solve(problem, uniform)
+    elif choice == 2:
+        search.solve(problem, misplace)
+    elif choice == 3:
+        search.solve(problem, euclid)
 
 
 
 def main():
-    problem = Problem(menu())
-    problem.print()
-    #print(problem.getGoalState())
-    moves = problem.getLegalOps()
-    print(moves)
+    #problem.print()
 
-    uniform = UniformHeur()
-    misplace = MisplacedHeur()
-    euclid = EuclidHeur()
-    #print(misplace.calculate(problem.getInitState(), problem.getGoalState()))
-    #print(euclid.calculate(problem.getInitState(), problem.getGoalState()))
-    #print(uniform.calculate(problem.getInitState(), problem.getGoalState()))
+    search.solve(problem, euclid)
 
 if __name__ == "__main__":
     main()
